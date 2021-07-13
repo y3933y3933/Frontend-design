387f8f61f5f708f311cb74e07722f0fcf1dbc76d
y3933y3933 <y3933y3933@gmail.com>
2021-07-14 07:51:11 +0800

    feat: intro-component-with-signup-form 修改成scss、js form validate完成

--- intro-component-with-signup-form-master/index.js
+++ intro-component-with-signup-form-master/index.js
@@ -1,2 +1,35 @@
-const form = document.getElementById("form");
+const submit = document.getElementById("submit");
+const firstName = document.getElementById("firstname");
+const lastName = document.getElementById("lastname");
+const password = document.getElementById("password");
 
+firstName.addEventListener("input", function (e) {
+  notEmpty.call(this, e.target.value);
+});
+
+lastName.addEventListener("input", function (e) {
+  notEmpty.call(this, e.target.value);
+});
+
+password.addEventListener("input", function (e) {
+  notEmpty.call(this, e.target.value);
+});
+
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  checkInput();
+});
+
+function checkInput() {
+  notEmpty.call(firstName, firstName.value);
+  notEmpty.call(lastName, lastName.value);
+  notEmpty.call(password, password.value);
+}
+
+function notEmpty(value) {
+  if (!value.trim()) {
+    this.setCustomValidity("Required");
+  } else {
+    this.setCustomValidity("");
+  }
+}
